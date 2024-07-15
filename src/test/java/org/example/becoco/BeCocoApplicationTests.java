2077f692bae79728223528fdf49d60425f39cec2
in-jun <injuninjune@gmail.com>
2024-07-15 16:09:23 +0900

    first commit

---
+++ src/test/java/org/example/becoco/BeCocoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.becoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeCocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
