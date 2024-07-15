471007f09eda090b29675383a1c6dbdcc9b6aff5
in-jun <me@injun.dev>
2024-07-15 16:09:23 +0900

    first commit

---
+++ src/main/java/org/example/becoco/BeCocoApplication.java
@@ -0,0 +1,13 @@
+package org.example.becoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCocoApplication.class, args);
+	}
+
+}
