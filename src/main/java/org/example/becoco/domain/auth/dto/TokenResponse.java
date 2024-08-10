537314bb454eb7bbb0f94b40b06f96dc0c90d733
imhanseong <hans_hanseong@dsm.hs.kr>
2024-08-10 10:59:52 +0900

    refactor :: package 구조 변경

---
+++ src/main/java/org/example/becoco/domain/auth/dto/TokenResponse.java
@@ -0,0 +1,13 @@
+package org.example.becoco.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+    private Long accessTokenTime;
+}
---
+++ src/main/java/org/example/becoco/domain/auth/facade/AuthFacade.java
@@ -0,0 +1,18 @@
+package org.example.becoco.domain.auth.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.example.becoco.domain.auth.entity.Auth;
+import org.example.becoco.domain.auth.exception.UserNotFoundException;
+import org.example.becoco.domain.auth.repository.AuthRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class AuthFacade {
+    private final AuthRepository authRepository;
+
+    public Auth getCurrentUser() {
+        return authRepository.findByUserName(SecurityContextHolder.getContext().getAuthentication().getName()).orElseThrow(()-> UserNotFoundException.EXCEPTION);
+    }
+}
