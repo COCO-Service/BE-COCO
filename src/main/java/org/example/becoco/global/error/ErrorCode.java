91a3032d208f96dbdca7ae18cbe051d62fdf05dd
Hannah <sunday0206@dsm.hs.kr>
2024-08-16 12:24:29 +0900

    feat :: 브랜치 충돌 요소 제거6

--- src/main/java/org/example/becoco/global/error/ErrorCode.java
+++ src/main/java/org/example/becoco/global/error/ErrorCode.java
@@ -8,12 +8,9 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 public enum ErrorCode {
     USER_NOT_CORRECT(401, "USER NOT CORRECT."),
-    USER_NOT_FOUND(404, "USER NOT FOUND."),
-
     POST_NOT_FOUND(404, "POST NOT FOUND."),
     POST_NOT_DELETE(401, "POST Not Delete"),
     POST_NOT_UPDATE(401, "POST Not Update"),
-
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private final Integer httpStatus;
