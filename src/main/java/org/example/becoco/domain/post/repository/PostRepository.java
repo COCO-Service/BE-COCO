2d55eec91b7d0b861b1c8cd619b7c9cb6db1aa92
in-jun <me@injun.dev>
2024-08-16 15:04:09 +0900

    Revert "feat: user가 작성한 post찾는 메서드 추가"
    This reverts commit dc3bb5282f98a0d5b798533b0deaafae2a5dd61a.

--- src/main/java/org/example/becoco/domain/post/repository/PostRepository.java
+++ src/main/java/org/example/becoco/domain/post/repository/PostRepository.java
@@ -3,7 +3,6 @@ package org.example.becoco.domain.post.repository;
 import org.example.becoco.domain.post.dto.response.PostListElement;
 import org.example.becoco.domain.post.entity.Post;
 import org.example.becoco.domain.post.entity.type.Type;
-import org.example.becoco.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +11,4 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<PostListElement> findAllByType(Type type);
-    List<PostListElement> findAllByUser(User user);
 }
