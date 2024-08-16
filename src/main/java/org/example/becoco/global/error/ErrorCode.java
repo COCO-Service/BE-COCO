5d10088cb37064349241b6bc599e87ede2ac8afb
Hannah <sunday0206@dsm.hs.kr>
2024-08-16 17:27:12 +0900

    feat :: 병합 충돌 위치 찾기

--- src/main/java/org/example/becoco/global/error/ErrorCode.java
+++ src/main/java/org/example/becoco/global/error/ErrorCode.java
@@ -6,16 +6,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    POST_NOT_FOUND(404, "POST NOT FOUND."),
-    POST_NOT_DELETE(401, "POST Not Delete"),
-    POST_NOT_UPDATE(401, "POST Not Update"),
+//    POST_NOT_FOUND(404, "POST NOT FOUND."),
+//    POST_NOT_DELETE(401, "POST Not Delete"),
+//    POST_NOT_UPDATE(401, "POST Not Update"),
 
     USER_NOT_FOUND(404, "User Not Found"),
     COMMENT_NOT_FOUND(404, "Feed Not Found"),
     NICKNAME_ALREADY_EXISTS(409, "Nickname Already Exists"),
     PASSWORD_MISS_MATCH(400, "Password Miss Match"),
     USER_MISS_MATCH(403,"User Miss Match"),
-    USER_NOT_CORRECT(401, "USER NOT CORRECT."),
+//    USER_NOT_CORRECT(401, "USER NOT CORRECT."),
 
     JWT_EXPIRED(401, "Jwt Expired"),
     JWT_INVALID(401, "Jwt Invalid"),
