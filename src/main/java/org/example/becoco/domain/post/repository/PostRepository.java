7cfa1833611a5a8738267c92412c00e844c1bc1b
Hannah <sunday0206@dsm.hs.kr>
2024-08-16 17:34:48 +0900

    feat :: 충돌 해결

---
+++ src/main/java/org/example/becoco/domain/post/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package org.example.becoco.domain.post.repository;
+
+import org.example.becoco.domain.post.dto.response.PostListElement;
+import org.example.becoco.domain.post.entity.Post;
+import org.example.becoco.domain.post.entity.type.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<PostListElement> findAllByType(Type type);
+    List<PostListElement> findAllByUser(User user);
+}
