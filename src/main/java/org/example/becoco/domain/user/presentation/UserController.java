db6e70f3b8882b8243f9ce3d9aaa78cebf33e618
in-jun <injuninjune@gmail.com>
2024-08-18 01:36:04 +0900

    fix: Controller -> RestController

--- src/main/java/org/example/becoco/domain/user/presentation/UserController.java
+++ src/main/java/org/example/becoco/domain/user/presentation/UserController.java
@@ -5,11 +5,11 @@ import org.example.becoco.domain.post.presentation.dto.response.PostListResponse
 import org.example.becoco.domain.user.presentation.dto.response.UserInfoResponse;
 import org.example.becoco.domain.user.service.UserInfoService;
 import org.example.becoco.domain.user.service.UserPostsService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/mypage")
 public class UserController {
